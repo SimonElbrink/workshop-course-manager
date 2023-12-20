cab419c49f1567cbc2a1c1babae958df5eba666b
Goran Muminovic <goranmuminovic@gmail.com>
2023-12-20 09:54:34 +0100

    Course.java and Student.java getters and setters

--- src/main/java/se/lexicon/course_manager/model/Course.java
+++ src/main/java/se/lexicon/course_manager/model/Course.java
@@ -16,7 +16,43 @@ public class Course {
     ArrayList<Student> students = new ArrayList<>();
 
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    public String getCourseName() {
+        return courseName;
+    }
 
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration() {
+        return weekDuration;
+    }
+
+    public void setWeekDuration(int weekDuration) {
+        this.weekDuration = weekDuration;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
 }
