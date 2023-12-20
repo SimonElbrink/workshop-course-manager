6e16bb87f9835e4667971a15ececb85eea1d991d
Goran Muminovic <goranmuminovic@gmail.com>
2023-12-20 09:49:29 +0100

    Course.java and Student.java fields

--- src/main/java/se/lexicon/course_manager/model/Course.java
+++ src/main/java/se/lexicon/course_manager/model/Course.java
@@ -1,5 +1,22 @@
 package se.lexicon.course_manager.model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Course {
+
+    private int id;
+
+    private String courseName;
+
+    private LocalDate startDate;
+
+    private int weekDuration;
+
+    ArrayList<Student> students = new ArrayList<>();
+
+
+
+
 
 }
--- src/main/java/se/lexicon/course_manager/model/Student.java
+++ src/main/java/se/lexicon/course_manager/model/Student.java
@@ -2,4 +2,13 @@ package se.lexicon.course_manager.model;
 
 public class Student {
 
+    private int id;
+
+    private String name;
+
+    private String email;
+
+    private String address;
+
+
 }
