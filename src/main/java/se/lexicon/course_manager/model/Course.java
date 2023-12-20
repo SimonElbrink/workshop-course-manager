b5f8ebed22d6f6889a59b7213b01827e79951aff
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-12-20 10:06:04 +0100

    Initial commit  - Create Student and Course models

--- src/main/java/se/lexicon/course_manager/model/Course.java
+++ src/main/java/se/lexicon/course_manager/model/Course.java
@@ -1,5 +1,73 @@
 package se.lexicon.course_manager.model;
 
-public class Course {
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Objects;
 
+public class Course {
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private Collection<Student> students;
+
+    public Course() {
+    }
+
+    public Course(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && weekDuration == course.weekDuration && Objects.equals(courseName, course.courseName) && Objects.equals(startDate, course.startDate) && Objects.equals(students, course.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Course{");
+        sb.append("id=").append(id);
+        sb.append(", courseName='").append(courseName).append('\'');
+        sb.append(", startDate=").append(startDate);
+        sb.append(", weekDuration=").append(weekDuration);
+        sb.append(", students=").append(students);
+        sb.append('}');
+        return sb.toString();
+    }
 }
