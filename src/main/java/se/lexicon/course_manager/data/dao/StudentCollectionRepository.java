58b34c68570fceb61eb00c6733f0543a4367cd95
Goran Muminovic <goranmuminovic@gmail.com>
2023-12-20 15:59:00 +0100

    Methods in CourseCollectionRepository - all but findByStudentId

--- src/main/java/se/lexicon/course_manager/data/dao/StudentCollectionRepository.java
+++ src/main/java/se/lexicon/course_manager/data/dao/StudentCollectionRepository.java
@@ -5,8 +5,10 @@ package se.lexicon.course_manager.data.dao;
 import se.lexicon.course_manager.data.sequencers.StudentSequencer;
 import se.lexicon.course_manager.model.Student;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 
 
 public class StudentCollectionRepository implements StudentDao {
@@ -29,12 +31,26 @@ public class StudentCollectionRepository implements StudentDao {
 
     @Override
     public Student findByEmailIgnoreCase(String email) {
+        for (Student student : students) {
+            //if (students.contains(student.getEmail())) {
+            //    return student;
+            //}
+            if (student.getEmail().equalsIgnoreCase(email)) {
+                return student;
+            }
+        }
         return null;
     }
-
+    // if above is correct?
     @Override
     public Collection<Student> findByNameContains(String name) {
-        return null;
+        List<Student> result = new ArrayList<>();
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(name)) {
+                result.add(student);
+            }
+        }
+        return result;
     } // Do last
 
     @Override
@@ -49,21 +65,25 @@ public class StudentCollectionRepository implements StudentDao {
 
     @Override
     public Collection<Student> findAll() {
-        return null;
-    }
+        //return null;
+        return this.students; // do we need to iterate with a loop?
+        // return new array list (students).
 
+    }
+    // just return the whole Set?
     @Override
     public boolean removeStudent(Student student) {
-        if (students.contains(student)){
-            students.remove(student); // Is this enough? Just remove the object Student
+        //if (students.contains(student)){
+            //students.remove(student); // Is this enough? Just remove the object Student
             //which is passed in here? Do we need to worry about how the student will be
             //removed for example by id? That is probably already implemented and no need
             //to worry about it?
-            return true;
-        }
-        return false;
+            //return true;
+        //}
+        //return false;
+        return students.remove(student); // this always returns boolean.
     }
-
+    // ask if above is correct.
     @Override
     public void clear() {
         this.students = new HashSet<>();
