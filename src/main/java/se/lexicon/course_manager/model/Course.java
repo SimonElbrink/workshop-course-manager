769b2f9be72c542fee78f0a375f52682b8809895
Anita Ruth Sweetlin <anitaruthsweetlin@gmail.com>
2023-12-21 13:13:24 +0100

    Created model class - student, course

--- src/main/java/se/lexicon/course_manager/model/Course.java
+++ src/main/java/se/lexicon/course_manager/model/Course.java
@@ -1,5 +1,109 @@
 package se.lexicon.course_manager.model;
 
-public class Course {
+import se.lexicon.course_manager.data.sequencers.CourseSequencer;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+public class Course implements Serializable {
+
+    private int id;
+    private String courseName;
+    private LocalDate startDate;
+    private int weekDuration;
+    private Collection<Student> students;
+
+    public Course(String courseName, LocalDate startDate,int weekDuration) {
+        setId(CourseSequencer.nextCourseId());
+        setCourseName(courseName);
+        setStartDate(startDate);
+        setWeekDuration(weekDuration);
+        setStudents();
+    }
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    private void setCourseName(String courseName) {
+        if (courseName == null && courseName.isBlank()) throw new IllegalArgumentException("Course name was null");
+        this.courseName = courseName;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    private void setStartDate(LocalDate startDate) {
+        if (startDate == null) throw new IllegalArgumentException("Start date was null");
+        this.startDate = startDate;
+    }
+
+    public int getWeekDuration(){
+        return weekDuration;
+    }
+
+    private void setWeekDuration(int weekDuration){
+        if (weekDuration == 0) throw new IllegalArgumentException("Week duration was 0");
+        this.weekDuration = weekDuration;
+    }
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents() {
+        this.students = new ArrayList<Student>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && weekDuration == course.weekDuration && Objects.equals(courseName, course.courseName) && Objects.equals(startDate, course.startDate) && Objects.equals(students, course.students);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, courseName, startDate, weekDuration, students);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Course{");
+        sb.append("id=").append(id);
+        sb.append(", courseName='").append(courseName).append('\'');
+        sb.append(", startDate=").append(startDate);
+        sb.append(", weekDuration=").append(weekDuration);
+        sb.append(", students=").append(students);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    public boolean enrollStudent(Student student) {
+        if(this.students!= null && !this.students.contains(student)) {
+            students.add(student);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unrollStudent(Student student) {
+        if(this.students!= null && this.students.contains(student)){
+            students.remove(student);
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/se/lexicon/course_manager/model/Student.java
+++ src/main/java/se/lexicon/course_manager/model/Student.java
@@ -1,5 +1,81 @@
 package se.lexicon.course_manager.model;
 
-public class Student {
+import se.lexicon.course_manager.data.sequencers.StudentSequencer;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Student implements Serializable {
+
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+
+    public Student(String name, String email, String address) {
+        setId(StudentSequencer.nextStudentId());
+        setName(name);
+        setEmail(email);
+        setAddress(address);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if(name == null || name.isBlank()) throw new IllegalArgumentException("Name was null");
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private void setEmail(String email) {
+        if(email==null || email.isBlank()) throw new IllegalArgumentException("Email was null");
+        this.email = email;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    private void setAddress(String address) {
+        if(address==null || address.isBlank()) throw new IllegalArgumentException("Address was null");
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", Address='" + address + '\'' +
+                '}';
+    }
 
 }
